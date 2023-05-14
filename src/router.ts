760b5eb778e9a29366d830ba4104dda5bdcfcef2
kituuu <karthik.ayangar7118@gmail.com>
2023-05-14 16:39:46 +0530

    refracter: router

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,26 @@
 import {ping} from './scripts/ping';
+import {sastenashe} from './scripts/sastenashe';
+import {score} from './scripts/score';
 export const route = async (app: any) => {
-  app.message('ping', async ({message, say}: {message: any; say: Function}) => {
+  // Route for ping
+  app.message('sis ping', async ({say}: {say: Function}) => {
     let response: string = await ping();
     await say(response);
   });
+  // Route for saste nashe (testing)
+  app.message('sis saste nashe', async ({say}: {say: Function}) => {
+    let response: string = await sastenashe();
+    await say(response);
+  });
+  // Route for score
+  app.message(
+    'sis score',
+    async ({message, say}: {message: any; say: Function}) => {
+      let temp: any = message.text.toString();
+      temp = temp.split(' ');
+      let year: string = temp[temp.length - 1];
+      let response: object = await score(year);
+      await say(response);
+    }
+  );
 };
--- src/scripts/ping.ts
+++ src/scripts/ping.ts
@@ -1,12 +1,5 @@
-import {getPing, getValue, setValue} from '../services/redis';
+import {getPing} from '../services/redis';
 
-// import } from 'redis';
 export const ping = async () => {
-  // if (!getValue('ping')) {
-  //   return getValue('ping');
-  // } else {
-  //   setValue('ping', 'pong');
-  //   return getValue('ping');
-  // }
   return await getPing();
 };
