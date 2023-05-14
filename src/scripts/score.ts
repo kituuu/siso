abd87b829caad832b1ea6ff6c210863eddcf3e92
kituuu <karthik.ayangar7118@gmail.com>
2023-05-15 01:37:53 +0530

    refractor+feat: sis score, sis ++/--
    Signed-off-by: kituuu <karthik.ayangar7118@gmail.com>

--- src/scripts/score.ts
+++ src/scripts/score.ts
@@ -1,8 +1,12 @@
-import {getValue} from '../services/redis';
+import {getValue, setValue} from '../services/redis';
 
 export const score = async (batch: string): Promise<object> => {
+  // Getting data from redis and parsing it
   let mdgData: any = JSON.parse(await getValue('mdg'));
-  console.log(mdgData);
+  // creating a block element's field array of all the rows
+  // header - heading
+  // divider : horizontal line
+  // section : Each section represents a row of the table
   let scoreDataArr: any = [
     {
       type: 'header',
@@ -14,40 +18,127 @@ export const score = async (batch: string): Promise<object> => {
     {
       type: 'divider',
     },
-    {
-      type: 'section',
-      fields: [
-        {
-          type: 'mrkdwn',
-          text: 'Name',
-        },
-        {
-          type: 'mrkdwn',
-          text: 'Score',
-        },
-      ],
-    },
   ];
-
+  var nameString: string = 'Name\n';
+  var scoreString: string = 'Score\n';
   for (const user in mdgData[batch]) {
-    console.log(mdgData[batch][user]['name']);
-    scoreDataArr.push({
-      type: 'section',
-      fields: [
-        {
-          type: 'mrkdwn',
-          text: String(mdgData[batch][user]['name']),
-        },
-        {
-          type: 'mrkdwn',
-          text: String(mdgData[batch][user]['score']),
-        },
-      ],
-    });
+    nameString = nameString + String(mdgData[batch][user]['name']) + '\n';
+    scoreString = scoreString + String(mdgData[batch][user]['score']) + '\n';
+
+    // console.log(scoreString);
+    // scoreDataArr.push({
+    //   type: 'section',
+    //   fields: [
+    //     {
+    //       type: 'mrkdwn',
+    //       text: String(mdgData[batch][user]['name']),
+    //     },
+    //     {
+    //       type: 'mrkdwn',
+    //       text: String(mdgData[batch][user]['score']),
+    //     },
+    //   ],
+    // });
+  }
+  scoreDataArr.push({
+    type: 'section',
+    fields: [
+      {
+        type: 'mrkdwn',
+        text: nameString,
+      },
+      {
+        type: 'mrkdwn',
+        text: scoreString,
+      },
+    ],
+  });
+  let blockData: Object = {
+    blocks: scoreDataArr,
+  };
+  return blockData;
+};
+
+const plusplus = async (user: string): Promise<any> => {
+  let mdgData: any = JSON.parse(await getValue('mdg'));
+  for (const batch in mdgData) {
+    for (const usr in mdgData[batch]) {
+      if (mdgData[batch][usr]['slackID'] == user) {
+        // console.log(mdgData[batch][usr]);
+        mdgData[batch][usr]['score'] = mdgData[batch][usr]['score'] + 1;
+        // console.log(mdgData[batch][usr]);
+        setValue('mdg', JSON.stringify(mdgData));
+        return await [
+          mdgData[batch][usr]['displayName'],
+          mdgData[batch][usr]['score'],
+        ];
+      }
+    }
+  }
+};
+
+const minusminus = async (user: string): Promise<any> => {
+  let mdgData: any = JSON.parse(await getValue('mdg'));
+  for (const batch in mdgData) {
+    for (const usr in mdgData[batch]) {
+      if (mdgData[batch][usr]['slackID'] == user) {
+        // console.log(mdgData[batch][usr]);
+        mdgData[batch][usr]['score'] = mdgData[batch][usr]['score'] - 1;
+        // console.log(mdgData[batch][usr]);
+        setValue('mdg', JSON.stringify(mdgData));
+        return await [
+          mdgData[batch][usr]['displayName'],
+          mdgData[batch][usr]['score'],
+        ];
+      }
+    }
+  }
+};
+
+export const handleScoreUpdate = async (message: string): Promise<any> => {
+  // To store all the fields of block ele
+  let blockArr: any = [];
+  let temp: string[] = message.split(' ');
+  let userPlusPlus: string[] = [];
+  let userMinusMinus: string[] = [];
+  for (let i: number = 0; i < temp.length - 1; i++) {
+    if (temp[i].match(/<@[0-9a-zA-Z]*>/) != null) {
+      if (temp[i + 1] == '++') {
+        let response: any = await plusplus(
+          temp[i].slice(2, temp[i].length - 1)
+        );
+        blockArr.push({
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: String(
+                `${response[0]}++ [Splendid! You're now at ${response[1]}]`
+              ),
+            },
+          ],
+        });
+      } else if (temp[i + 1] == '--') {
+        let response: any = await minusminus(
+          temp[i].slice(2, temp[i].length - 1)
+        );
+        blockArr.push({
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: String(
+                `${response[0]}-- [Ouch! You're now at ${response[1]}]`
+              ),
+            },
+          ],
+        });
+      }
+    }
   }
 
   let blockData: Object = {
-    blocks: scoreDataArr,
+    blocks: blockArr,
   };
   return blockData;
 };
