78092e460bc258b8734e89cc0ec90c4edc2a9119
kituuu <karthik.ayangar7118@gmail.com>
2023-05-18 13:16:06 +0530

    refractor: declared type of App
    Signed-off-by: kituuu <karthik.ayangar7118@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,16 @@ dotenv.config();
 import {App} from '@slack/bolt';
 import {redisInit} from './services/redis';
 import {route} from './router';
+import {StringIndexed} from '@slack/bolt/dist/types/helpers';
 
-const app = new App({
+const app: App<StringIndexed> = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true, // add this
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
-async function init() {
+async function init(): Promise<void> {
   await app.start(process.env.PORsT || 3000);
   console.log('[Kaori] is happy, yikes :D');
   await redisInit();
--- src/router.ts
+++ src/router.ts
@@ -1,48 +1,41 @@
+// import { App } from '@slack/bolt';
 import {help} from './scripts/help';
 import {getInfo} from './scripts/info';
 import {ping} from './scripts/ping';
 import {handleScoreUpdate, score} from './scripts/score';
-export const route = async (app: any) => {
+import {App, AppMessage} from './types';
+export const route = async (app: App) => {
   // Route for ping
-  app.message('sis ping', async ({say}: {say: Function}) => {
+  app.message('sis ping', async ({say}: AppMessage) => {
     let response: string = await ping();
     await say(response);
   });
 
   // Route for score
-  app.message(
-    'sis score',
-    async ({message, say}: {message: {text: string}; say: Function}) => {
-      let year: string = message.text.toString().split(' ')[
-        message.text.toString().split(' ').length - 1
-      ];
-      let response: object = await score(year);
-      await say(response);
-    }
-  );
+  app.message('sis score', async ({message, say}: AppMessage) => {
+    let year: string = message.text.toString().split(' ')[
+      message.text.toString().split(' ').length - 1
+    ];
+    let response: object = await score(year);
+    await say(response);
+  });
   // Route for ++/-- (score updation)
-  app.message(
-    '',
-    async ({message, say}: {message: {text: string}; say: Function}) => {
-      if (
-        message.text.includes('sis') &&
-        (message.text.includes('++') || message.text.includes('--'))
-      ) {
-        await say(await handleScoreUpdate(message.text));
-      }
+  app.message('', async ({message, say}: AppMessage) => {
+    if (
+      message.text.includes('sis') &&
+      (message.text.includes('++') || message.text.includes('--'))
+    ) {
+      await say(await handleScoreUpdate(message.text));
     }
-  );
+  });
 
   // Route for sis info
-  app.message(
-    'sis info',
-    async ({message, say}: {message: {text: string}; say: Function}) => {
-      let response: object = await getInfo(message.text);
-      say(response);
-    }
-  );
+  app.message('sis info', async ({message, say}: AppMessage) => {
+    let response: object = await getInfo(message.text);
+    say(response);
+  });
   // Route for sis helf
-  app.message('sis help', async ({say}: {say: Function}) => {
+  app.message('sis help', async ({say}: AppMessage) => {
     let response: object = await help();
     say(response);
   });
