d9c9ba2ccfbabf6ac6370175d99139dc1aa235d4
kituuu <karthik.ayangar7118@gmail.com>
2023-05-18 07:20:53 +0530

    refractor: adds no user found check
    Signed-off-by: kituuu <karthik.ayangar7118@gmail.com>

--- src/scripts/score.ts
+++ src/scripts/score.ts
@@ -1,12 +1,13 @@
 import {getValue, setValue} from '../services/redis';
+import {Data} from '../types';
 
 export const score = async (batch: string): Promise<object> => {
   // Getting data from redis and parsing it
-  let mdgData: any = JSON.parse(await getValue('mdg'));
+  let mdgData: Data = JSON.parse(await getValue('mdg'));
   let nameString: string = 'Name\n';
   let scoreString: string = 'Score\n';
   // Boilerplate/protype of block components
-  let scoreDataArr: any = [
+  let scoreDataArr: object[] = [
     {
       type: 'header',
       text: {
@@ -20,8 +21,8 @@ export const score = async (batch: string): Promise<object> => {
   ];
 
   for (const user in mdgData[batch]) {
-    nameString = nameString + String(mdgData[batch][user]['name']) + '\n';
-    scoreString = scoreString + String(mdgData[batch][user]['score']) + '\n';
+    nameString = nameString + mdgData[batch][user]['name'] + '\n';
+    scoreString = scoreString + mdgData[batch][user]['score'] + '\n';
   }
   // adding names and scores to seperate section of block component
   scoreDataArr.push({
@@ -43,48 +44,48 @@ export const score = async (batch: string): Promise<object> => {
   };
 };
 
-const plusplus = async (user: string): Promise<any> => {
-  let mdgData: any = JSON.parse(await getValue('mdg'));
+const plusplus = async (user: string): Promise<(string | number)[]> => {
+  let mdgData: Data = JSON.parse(await getValue('mdg'));
+  let result: (string | number)[] = ['Not Found', 0];
   for (const batch in mdgData) {
     for (const usr in mdgData[batch]) {
       if (mdgData[batch][usr]['slackID'] == user) {
         mdgData[batch][usr]['score'] = mdgData[batch][usr]['score'] + 1;
         setValue('mdg', JSON.stringify(mdgData));
-        return await [
-          mdgData[batch][usr]['displayName'],
-          mdgData[batch][usr]['score'],
-        ];
+        result[0] = mdgData[batch][usr]['displayName'];
+        result[1] = mdgData[batch][usr]['score'];
       }
     }
   }
+  return await result;
 };
 
-const minusminus = async (user: string): Promise<any> => {
-  let mdgData: any = JSON.parse(await getValue('mdg'));
+const minusminus = async (user: string): Promise<(string | number)[]> => {
+  let mdgData: Data = JSON.parse(await getValue('mdg'));
+  let result: (string | number)[] = ['Not Found', 0];
   for (const batch in mdgData) {
     for (const usr in mdgData[batch]) {
       if (mdgData[batch][usr]['slackID'] == user) {
         mdgData[batch][usr]['score'] = mdgData[batch][usr]['score'] - 1;
         setValue('mdg', JSON.stringify(mdgData));
-        return await [
-          mdgData[batch][usr]['displayName'],
-          mdgData[batch][usr]['score'],
-        ];
+        result[0] = mdgData[batch][usr]['displayName'];
+        result[1] = mdgData[batch][usr]['score'];
       }
     }
   }
+  return await result;
 };
 
 export const handleScoreUpdate = async (message: string): Promise<object> => {
   // To store all the fields of block ele
-  let blockArr: any = [];
+  let blockArr: object[] = [];
   let temp: string[] = message.split(' ');
   for (let i: number = 0; i < temp.length - 1; i++) {
     // using regular expression to check if any user is tagged or not
     if (temp[i].match(/<@[0-9a-zA-Z]*>/) != null) {
       if (temp[i + 1] == '++') {
         // if ++ is used plusplus function is called which return name displayname and new score
-        let response: any = await plusplus(
+        let response: (string | number)[] = await plusplus(
           temp[i].slice(2, temp[i].length - 1)
         );
         // pushing it to new section of the block
@@ -101,7 +102,7 @@ export const handleScoreUpdate = async (message: string): Promise<object> => {
         });
       } else if (temp[i + 1] == '--') {
         // if -- (same logic as that of)
-        let response: any = await minusminus(
+        let response: (string | number)[] = await minusminus(
           temp[i].slice(2, temp[i].length - 1)
         );
         blockArr.push({
@@ -119,7 +120,7 @@ export const handleScoreUpdate = async (message: string): Promise<object> => {
     }
   }
 
-  let blockData: Object = {
+  let blockData: object = {
     blocks: blockArr,
   };
   return blockData;
